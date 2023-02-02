90ff6bb08e9ca4733bae86b99663f097f9b3cb78
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-02-02 01:32:57 +0100

    Cleanup project structure (invalid folders0

--- Logic/Operations/Interfaces/IArithmetic.cs
+++ Logic/Operations/Interfaces/IArithmetic.cs
@@ -11,30 +11,30 @@
         /// <param name="firstNumber">The first number.</param>
         /// <param name="secondNumber">The second number.</param>
         /// <returns>The result (sum) of adding two numbers.</returns>
-        double Add(double firstNumber, double secondNumber);
-        
+        public double Add(double firstNumber, double secondNumber);
+
         /// <summary>
         /// Subtracts two numbers from each other.
         /// </summary>
         /// <param name="firstNumber">The first number.</param>
         /// <param name="secondNumber">The second number.</param>
         /// <returns>The result (difference) of subtracting two numbers.</returns>
-        double Subtract(double firstNumber, double secondNumber);
-        
+        public double Subtract(double firstNumber, double secondNumber);
+
         /// <summary>
         /// Multiplies two numbers by each other.
         /// </summary>
         /// <param name="firstNumber">The first number.</param>
         /// <param name="secondNumber">The second number.</param>
         /// <returns>The result (product) of multiplying two numbers.</returns>
-        double Multiply(double firstNumber, double secondNumber);
-        
+        public double Multiply(double firstNumber, double secondNumber);
+
         /// <summary>
         /// Divides two numbers by each other.
         /// </summary>
         /// <param name="firstNumber">The first number.</param>
         /// <param name="secondNumber">The second number.</param>
         /// <returns>The result (quotient) of dividing two numbers.</returns>
-        double Divide(double firstNumber, double secondNumber);
+        public double Divide(double firstNumber, double secondNumber);
     }
 }
