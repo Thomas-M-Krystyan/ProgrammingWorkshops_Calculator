9e708c08a2f6c93613614875081efad09abd4e3b
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-02-05 01:51:01 +0100

    Add tests for GetLongestMethodName property in RegisterService

--- Tests/UnitTests/Application/Services/RegisterServiceTests.cs
+++ Tests/UnitTests/Application/Services/RegisterServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Calculator_Console.Services.Implementation;
 using Calculator_Console.Services.Interfaces;
+using Moq;
 using NUnit.Framework;
 using Operations.Implementation;
 using Operations.Interfaces;
@@ -31,6 +32,39 @@ namespace Calculator_ConsoleTests.Services
 
             // Assert
             Assert.That(methods, Has.Count.EqualTo(CurrentMethodsAmount));
+        }
+
+        [Test]
+        public void CheckProperty_GetLongestMethodName_ReturnsExpectedLengthOfTheLastMethodNumber()
+        {
+            // Arrange #1
+            Mock<IRegisterService> registerMock = new();
+
+            registerMock
+                .SetupGet(mock => mock.Methods)
+                .Returns(new Dictionary<ushort, MethodInfo>());
+
+            // Act #1
+            IDictionary<ushort, MethodInfo> actualMethods = registerMock.Object.Methods;
+            ushort firstResult = registerMock.Object.GetLongestMethodName;
+
+            // Assert #1
+            Assert.That(actualMethods, Is.Empty);  // Mocked register is using injected dummy IArithmetic service
+            Assert.That(firstResult, Is.Zero);     // Generating the first result
+
+            // Arrange #2
+            registerMock
+                .SetupGet(mock => mock.Methods)
+                .Returns(new Dictionary<ushort, MethodInfo> { { 0, null } });  // Modify the collection
+
+            // Act #2
+            actualMethods = registerMock.Object.Methods;
+            ushort secondResult = registerMock.Object.GetLongestMethodName;  // Run the method again
+
+            // Assert #2
+            Assert.That(actualMethods, Has.Count.EqualTo(1));
+            Assert.That(secondResult, Is.Zero);  // The result should be cached even if the collection was modified
+                                                                 // Normally such change would't be possible because methods are scanned once
         }
 
         [Test]
